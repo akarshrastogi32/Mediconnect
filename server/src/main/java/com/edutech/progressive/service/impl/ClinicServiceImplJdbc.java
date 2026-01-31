0b45f6111d43b06c5354417a9709c8dbd3d153ed
akarshrastogi32 <dummy@gmail.com>
2026-01-31 04:05:54 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJdbc.java
@@ -0,0 +1,37 @@
+package com.edutech.progressive.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edutech.progressive.entity.Clinic;
+import com.edutech.progressive.service.ClinicService;
+
+public class ClinicServiceImplJdbc implements ClinicService {
+
+    @Override
+    public List<Clinic> getAllClinics() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Clinic getClinicById(int clinicId) {
+       return null;
+    }
+
+    @Override
+    public Integer addClinic(Clinic clinic) {
+        return -1;
+    }
+
+    @Override
+    public void updateClinic(Clinic clinic) {
+        
+    }
+
+    @Override
+    public void deleteClinic(int clinicId) {
+        
+    }
+    
+
+}
---
+++ server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJdbc.java
@@ -0,0 +1,39 @@
+package com.edutech.progressive.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edutech.progressive.entity.Doctor;
+import com.edutech.progressive.service.DoctorService;
+
+public class DoctorServiceImplJdbc implements DoctorService  {
+
+    @Override
+    public List<Doctor> getAllDoctors() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Integer addDoctor(Doctor doctor) {
+       return -1;
+    }
+
+    @Override
+    public List<Doctor> getDoctorSortedByExperience() {
+        return new ArrayList<>();
+    }
+    public Doctor getDoctorById(int doctorId)
+    {
+        return null;
+
+    }
+    public void updateDoctor(Doctor doctor)
+    {
+
+    }
+    public void deleteDoctor(int doctorId)
+    {
+        
+    }
+
+}
