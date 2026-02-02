79c051fa5dd53de4b1859b74f14cfaeaff5f63f9
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-02 22:02:23 +0530

    Update PatientServiceImplJdbc.java

--- server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJdbc.java
@@ -1,38 +1,76 @@
 package com.edutech.progressive.service.impl;
 
-import java.util.ArrayList;
+
+import java.util.Collections;
 import java.util.List;
 
+import com.edutech.progressive.dao.PatientDAO;
 import com.edutech.progressive.entity.Patient;
 import com.edutech.progressive.service.PatientService;
 
-public class PatientServiceImplJdbc  implements PatientService{
+public class PatientServiceImplJdbc implements PatientService {
 
-    @Override
-    public List<Patient> getAllPatients() {
-        return new ArrayList<>();
+    private PatientDAO patientDAO;
+
+    public PatientServiceImplJdbc(PatientDAO patientDAO) {
+        this.patientDAO = patientDAO;
     }
 
     @Override
-    public Integer addPatient(Patient patient) {
-        return -1;
+    public List<Patient> getAllPatients() throws Exception {
+        List<Patient> patients = null;
+        try {
+            patients = patientDAO.getAllPatients();
+        } catch (Exception e) {
+            throw e;
+        }
+
+        return patients;
     }
 
     @Override
-    public List<Patient> getAllPatientSortedByName() {
-        return new ArrayList<>();
+    public Integer addPatient(Patient patient) throws Exception {
+        try {
+            return patientDAO.addPatient(patient);
+        } catch (Exception e) {
+            throw e;
+        }
     }
-    public void updatePatient(Patient patient)
-    {
 
+    @Override
+    public List<Patient> getAllPatientSortedByName() throws Exception {
+        List<Patient> patients = null;
+        try {
+            patients = patientDAO.getAllPatients();
+            Collections.sort(patients);
+        } catch (Exception e) {
+            throw e;
+        }
+        return patients;
     }
-    public void deletePatient(Patient patient)
-    {
 
+    public void updatePatient(Patient patient) throws Exception {
+        try {
+            patientDAO.updatePatient(patient);
+        } catch (Exception e) {
+            throw e;
+        }
     }
-    public Patient getPatientById(int patientId)
-    {
-        return null;
+
+    public void deletePatient(int patientId) throws Exception {
+        try {
+            patientDAO.deletePatient(patientId);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Patient getPatientById(int patientId) throws Exception{
+          try {
+          return patientDAO.getPatientById(patientId);
+        } catch (Exception e) {
+        throw e;
+        }
     }
 
 }
