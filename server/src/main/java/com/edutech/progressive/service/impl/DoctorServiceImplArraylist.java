2cd093aef42e2b68da5b7a7d376ac1f399491a0c
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-02 21:47:58 +0530

    Update DoctorServiceImplArraylist.java

--- server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplArraylist.java
@@ -1,7 +1,6 @@
 package com.edutech.progressive.service.impl;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,32 +8,24 @@ import com.edutech.progressive.entity.Doctor;
 import com.edutech.progressive.service.DoctorService;
 
 public class DoctorServiceImplArraylist implements DoctorService {
-    private static List<Doctor> doctorList= new ArrayList<>();
 
-
-    // public void emptyArrayList()
-    // {
-    //     doctorList.clear();
-        
-    // }
+    private static List<Doctor> doctorList = new ArrayList<>();
 
     @Override
     public List<Doctor> getAllDoctors() {
         return doctorList;
-        
     }
-
     @Override
     public Integer addDoctor(Doctor doctor) {
-       
         doctorList.add(doctor);
         return doctor.getDoctorId();
+
     }
 
     @Override
     public List<Doctor> getDoctorSortedByExperience() {
-       Collections.sort(doctorList);
-       return doctorList;
+      Collections.sort(doctorList);
+      return doctorList;
     }
 
 }
