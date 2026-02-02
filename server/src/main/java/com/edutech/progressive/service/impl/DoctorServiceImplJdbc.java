59c7705a40d78228d4181d14067f9572b33de3c8
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-02 21:48:28 +0530

    Update DoctorServiceImplJdbc.java

--- server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJdbc.java
@@ -1,39 +1,86 @@
 package com.edutech.progressive.service.impl;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import com.edutech.progressive.dao.DoctorDAO;
 import com.edutech.progressive.entity.Doctor;
 import com.edutech.progressive.service.DoctorService;
 
-public class DoctorServiceImplJdbc implements DoctorService  {
+public class DoctorServiceImplJdbc  implements DoctorService{
 
-    @Override
-    public List<Doctor> getAllDoctors() {
-        return new ArrayList<>();
+      private DoctorDAO doctorDAO;
+
+    
+
+    public DoctorServiceImplJdbc(DoctorDAO doctorDAO) {
+        this.doctorDAO = doctorDAO;
     }
 
     @Override
-    public Integer addDoctor(Doctor doctor) {
-       return -1;
+    public List<Doctor> getAllDoctors() throws Exception  {
+       List<Doctor> doctors=null;
+        try {
+            doctors= doctorDAO.getAllDoctors();
+        } catch (Exception e) {
+           throw e;
+        }
+     
+        return doctors;
     }
 
     @Override
-    public List<Doctor> getDoctorSortedByExperience() {
-        return new ArrayList<>();
-    }
-    public Doctor getDoctorById(int doctorId)
-    {
-        return null;
+    public Integer addDoctor(Doctor doctor) throws Exception  {
+         try {
 
+            Integer id=doctorDAO.addDoctor(doctor);
+            if(id!=null && id>0){
+                doctor.setDoctorId(id);
+            }
+             return id;
+        } catch (Exception e) {
+          throw e;
+        }
     }
-    public void updateDoctor(Doctor doctor)
-    {
 
-    }
-    public void deleteDoctor(int doctorId)
-    {
+    @Override
+    public List<Doctor> getDoctorSortedByExperience()throws Exception  {
+        List<Doctor> doctors=null;
+        try {
+            doctors= doctorDAO.getAllDoctors();
+             Collections.sort(doctors);
+            
+        } catch (Exception e) {
+           throw e;
+        }
+     
+        return doctors;
         
     }
+
+     public void updateDoctor(Doctor doctor) throws Exception {
+        try {
+            doctorDAO.updateDoctor(doctor);;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void deleteDoctor(int doctorId) throws Exception {
+        try {
+            doctorDAO.deleteDoctor(doctorId);;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Doctor getDoctorById(int doctorId) throws Exception{
+          try {
+          return doctorDAO.getDoctorById(doctorId);
+        } catch (Exception e) {
+        throw e;
+        }
+    }
+
 
 }
