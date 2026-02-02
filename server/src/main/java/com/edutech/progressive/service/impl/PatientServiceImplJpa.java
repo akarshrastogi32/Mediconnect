cd124b39671f5b11131163057c0cd6626c7ae816
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-02 22:02:54 +0530

    Update PatientServiceImplJpa.java

--- server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJpa.java
@@ -1,5 +1,77 @@
 package com.edutech.progressive.service.impl;
 
-public class PatientServiceImplJpa  {
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edutech.progressive.entity.Patient;
+import com.edutech.progressive.exception.PatientNotFoundException;
+import com.edutech.progressive.repository.PatientRepository;
+import com.edutech.progressive.service.PatientService;
+
+@Service
+public class PatientServiceImplJpa implements PatientService  {
+
+    
+   private final PatientRepository patientRepository;
+    
+
+    public PatientServiceImplJpa(PatientRepository patientRepository ){
+        this.patientRepository=patientRepository;
+    }
+
+    
+    @Override
+    public List<Patient> getAllPatients() {
+     return patientRepository.findAll();
+    }
+
+    @Override
+    public Integer addPatient(Patient patient){
+       Patient p= patientRepository.save(patient);
+        return p.getPatientId();
+    }
+
+    @Override
+    public Patient getPatientById(int patientId)throws Exception {
+        return patientRepository.findByPatientId(patientId).orElse(null);
+    }
+
+
+    public void updatePatient(Patient patient) throws Exception{
+
+        if(patientRepository.existsById(patient.getPatientId())){
+
+        Patient p = getPatientById(patient.getPatientId());
+        p.setAddress(patient.getAddress());
+        p.setContactNumber(patient.getContactNumber());
+        p.setDateOfBirth(patient.getDateOfBirth());
+        p.setEmail(patient.getEmail());
+        p.setFullName(patient.getFullName());
+        patientRepository.save(p); 
+    
+    }
+
+    }
+
+    public void deletePatient(int patientId)throws Exception {
+        if(patientRepository.existsById(patientId)){
+        patientRepository.deleteById(patientId);
+    }
+       
+    }
+
+    @Override
+    public List<Patient> getAllPatientSortedByName() throws Exception {
+        List<Patient> patients = patientRepository.findAll();
+        Collections.sort(patients);
+        return patients;
+    }
+
+   
+
+  
 
 }
