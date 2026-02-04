213e731fc59353d9a693a950cf159903ff025b75
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-04 22:22:21 +0530

    Update ClinicController.java

--- server/src/main/java/com/edutech/progressive/controller/ClinicController.java
+++ server/src/main/java/com/edutech/progressive/controller/ClinicController.java
@@ -1,38 +1,65 @@
 package com.edutech.progressive.controller;
 
 import com.edutech.progressive.entity.Clinic;
+import com.edutech.progressive.service.ClinicService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/clinic")
 public class ClinicController {
-    public ResponseEntity<List<Clinic>> getAllClinics() {
-        return null;
+
+    @Autowired
+    ClinicService clinicService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Clinic>> getAllClinics() throws Exception {
+        return ResponseEntity.ok(clinicService.getAllClinics());
     }
 
-    public ResponseEntity<Clinic> getClinicById(int clinicId) {
-        return null;
+    @GetMapping("/{clinicId}")
+    public ResponseEntity<Clinic> getClinicById(@PathVariable int clinicId) throws Exception {
+        return ResponseEntity.ok(clinicService.getClinicById(clinicId));
     }
 
-    public ResponseEntity<Integer> addClinic(Clinic clinic) {
-        return null;
+    @PostMapping
+    public ResponseEntity<Integer> addClinic(@RequestBody Clinic clinic) throws Exception {
+        return new ResponseEntity<>(clinicService.addClinic(clinic),HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Void> updateClinic(int clinicId, Clinic clinic) {
-        return null;
+    @PutMapping("/{clinicId}")
+    public ResponseEntity<Void> updateClinic(@PathVariable int clinicId,@RequestBody Clinic clinic) throws Exception {
+        clinic.setClinicId(clinicId);
+        clinicService.updateClinic(clinic);
+        return ResponseEntity.ok().build();
     }
 
-    public ResponseEntity<Void> deleteClinic(int clinicId) {
-        return null;
+    @DeleteMapping("/{clinicId}")
+    public ResponseEntity<Void> deleteClinic(@PathVariable int clinicId) throws Exception {
+        clinicService.deleteClinic(clinicId);
+        return ResponseEntity.ok().build();
     }
 
-    public ResponseEntity<List<Clinic>> getAllClinicByLocation(String location) {
-        return null;
+    @GetMapping("/location/{location}")
+    public ResponseEntity<List<Clinic>> getAllClinicByLocation(@PathVariable String location) throws Exception {
+        return ResponseEntity.ok(clinicService.getAllClinicByLocation(location));
     }
 
-    public ResponseEntity<List<Clinic>> getAllClinicByDoctorId(int doctorId) {
-        return null;
+    @GetMapping("/doctor/{doctorId}")
+    public ResponseEntity<List<Clinic>> getAllClinicByDoctorId(@PathVariable int doctorId) throws Exception {
+        return ResponseEntity.ok(clinicService.getAllClinicByDoctorId(doctorId));
     }
 }
