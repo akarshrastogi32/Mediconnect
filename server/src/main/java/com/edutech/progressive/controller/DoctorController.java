81399b2d3f9843c9377fe6455c7fb2d13c1ef4a3
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-04 22:22:42 +0530

    Update DoctorController.java

--- server/src/main/java/com/edutech/progressive/controller/DoctorController.java
+++ server/src/main/java/com/edutech/progressive/controller/DoctorController.java
@@ -1,35 +1,61 @@
 package com.edutech.progressive.controller;
 
 import com.edutech.progressive.entity.Doctor;
+import com.edutech.progressive.service.DoctorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/doctor")
 public class DoctorController {
 
-    public ResponseEntity<List<Doctor>> getAllDoctors() {
-        return null;
+    @Autowired
+    DoctorService doctorService;
+
+    @GetMapping
+    public ResponseEntity<List<Doctor>> getAllDoctors() throws Exception {
+        return ResponseEntity.ok(doctorService.getAllDoctors());
     }
 
-    public ResponseEntity<Doctor> getDoctorById(int doctorId) {
-        return null;
+    @GetMapping("/{doctorId}")
+    public ResponseEntity<Doctor> getDoctorById(@PathVariable int doctorId) throws Exception {
+        return ResponseEntity.ok(doctorService.getDoctorById(doctorId));
     }
 
-    public ResponseEntity<Integer> addDoctor(Doctor doctor) {
-        return null;
+    @PostMapping
+    public ResponseEntity<Integer> addDoctor(@RequestBody Doctor doctor) throws Exception {
+        return ResponseEntity.status(201).body(doctorService.addDoctor(doctor));
     }
 
-    public ResponseEntity<Void> updateDoctor(int doctorId, Doctor doctor) {
-        return null;
+
+
+    @PutMapping("/{doctorId}")
+    public ResponseEntity<Void> updateDoctor(@PathVariable int doctorId, @RequestBody Doctor doctor) throws Exception {
+        doctor.setDoctorId(doctorId);
+        doctorService.updateDoctor(doctor);
+        return ResponseEntity.ok().build(); 
     }
 
-    public ResponseEntity<Void> deleteDoctor(int doctorId) {
-        return null;
+    @DeleteMapping("/{doctorId}")
+    public ResponseEntity<Void> deleteDoctor(@PathVariable int doctorId) throws Exception {
+        doctorService.deleteDoctor(doctorId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    public ResponseEntity<List<Doctor>> getDoctorSortedByExperience() {
-        return null;
+    @GetMapping("/experience")
+    public ResponseEntity<List<Doctor>> getDoctorSortedByExperience() throws Exception {
+        return ResponseEntity.ok(doctorService.getDoctorSortedByExperience());
     }
 }
