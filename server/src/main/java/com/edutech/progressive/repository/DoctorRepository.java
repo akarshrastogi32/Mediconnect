ecd9766d2e6c16943563abcefde92eba57bf43fd
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-04 22:31:12 +0530

    Update DoctorRepository.java

--- server/src/main/java/com/edutech/progressive/repository/DoctorRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/DoctorRepository.java
@@ -1,5 +1,16 @@
 package com.edutech.progressive.repository;
 
+import java.util.Optional;
 
-public interface DoctorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.edutech.progressive.entity.Doctor;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor,Integer>{
+
+    @Query("select d from Doctor d where d.doctorId=?1")
+    Optional<Doctor> findByDoctorId(int doctorId);
 }
