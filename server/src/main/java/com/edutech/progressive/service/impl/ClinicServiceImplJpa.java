c2c2cb66b14ddcae2cfacf6c2dd3dedd4ca488e2
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-04 22:34:25 +0530

    Update ClinicServiceImplJpa.java

--- server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJpa.java
@@ -2,42 +2,54 @@ package com.edutech.progressive.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edutech.progressive.entity.Clinic;
+import com.edutech.progressive.repository.ClinicRepository;
 import com.edutech.progressive.service.ClinicService;
 
 @Service
-public class ClinicServiceImplJpa implements ClinicService  {
+public class ClinicServiceImplJpa implements ClinicService {
+
+    private final ClinicRepository clinicRepository;
+
+    public ClinicServiceImplJpa(ClinicRepository clinicRepository) {
+        this.clinicRepository = clinicRepository;
+    }
 
     @Override
     public List<Clinic> getAllClinics() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllClinics'");
+        return clinicRepository.findAll();
     }
 
     @Override
     public Clinic getClinicById(int clinicId) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getClinicById'");
+        return clinicRepository.findById(clinicId).orElse(null);
     }
 
     @Override
     public Integer addClinic(Clinic clinic) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addClinic'");
+        Clinic c = clinicRepository.save(clinic);
+        return c.getClinicId();
     }
 
     @Override
     public void updateClinic(Clinic clinic) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateClinic'");
+        clinicRepository.save(clinic);
     }
 
     @Override
     public void deleteClinic(int clinicId) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteClinic'");
+        clinicRepository.deleteById(clinicId);
     }
 
+       public List<Clinic> getAllClinicByLocation(String location) throws Exception { 
+        return clinicRepository.findAllByLocation(location);
+        }
+
+    public List<Clinic> getAllClinicByDoctorId(int doctorId) throws Exception { 
+        return clinicRepository.findAllByDoctorId(doctorId);
+
+     }
 }
