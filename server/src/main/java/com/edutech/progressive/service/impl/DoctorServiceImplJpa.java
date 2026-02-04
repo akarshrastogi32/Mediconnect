3e6deae13ac2139c6860001d8bd0c0cd315010cf
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-04 22:35:54 +0530

    Update DoctorServiceImplJpa.java

--- server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJpa.java
@@ -1,32 +1,63 @@
 
 package com.edutech.progressive.service.impl;
 
+import java.util.Collections;
 import java.util.List;
 
+import javax.print.Doc;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edutech.progressive.entity.Doctor;
+import com.edutech.progressive.repository.DoctorRepository;
 import com.edutech.progressive.service.DoctorService;
 
 @Service
-public class DoctorServiceImplJpa  implements DoctorService {
+public class DoctorServiceImplJpa implements DoctorService {
+
+   private final DoctorRepository doctorRepository;
+
+    public DoctorServiceImplJpa(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
 
     @Override
     public List<Doctor> getAllDoctors() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllDoctors'");
+
+        return doctorRepository.findAll();
     }
 
     @Override
     public Integer addDoctor(Doctor doctor) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addDoctor'");
+       Doctor d=doctorRepository.save(doctor);
+        return d.getDoctorId();
     }
 
     @Override
     public List<Doctor> getDoctorSortedByExperience() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDoctorSortedByExperience'");
+
+        List<Doctor> list = doctorRepository.findAll();
+
+        Collections.sort(list);
+
+        return list;
+
+    }
+
+    public void updateDoctor(Doctor doctor) throws Exception {
+        doctorRepository.save(doctor);
+
+    }
+
+    public void deleteDoctor(int doctorId) throws Exception {
+        
+        doctorRepository.deleteById(doctorId);
+    }
+
+    public Doctor getDoctorById(int doctorId) throws Exception {
+        return doctorRepository.findByDoctorId(doctorId).get();
+
     }
 
 }
