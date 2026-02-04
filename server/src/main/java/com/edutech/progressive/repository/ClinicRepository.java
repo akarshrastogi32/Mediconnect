b5b96bc69aa7bbaf2c6f26f8c42209750f7cf11f
Akarsh Rastogi <rastogiaakarsh2003@gmail.com>
2026-02-04 22:30:48 +0530

    Update ClinicRepository.java

--- server/src/main/java/com/edutech/progressive/repository/ClinicRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/ClinicRepository.java
@@ -1,10 +1,22 @@
 package com.edutech.progressive.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.edutech.progressive.entity.Clinic;
 
 @Repository
-public interface ClinicRepository extends JpaRepository<Clinic,Integer> {
+public interface ClinicRepository extends JpaRepository<Clinic, Integer> {
+
+    Optional<Clinic> findByClinicId(Integer clinicId);
+
+    List<Clinic> findAllByLocation(String location);
+
+    List<Clinic> findAllByDoctorId(Integer doctorId);
+
+    void deleteByDoctorId(Integer doctorId);
+
 }
